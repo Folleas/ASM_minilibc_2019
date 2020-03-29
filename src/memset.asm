fd05c3aec6e6a654cec8289803d71cf3eecf68bc
Rayann <rayann.folleas@epitech.eu>
2020-03-29 19:26:56 +0200

    [ADD] memset

--- src/memset.asm
+++ src/memset.asm
@@ -3,24 +3,16 @@ SECTION .text
 GLOBAL memset
 
 memset:
-    mov rcx, 0
-    mov     rax, rdi
-    cmp     rdi, 0
-    je      exit_failure
     mov     rcx, rsi
+    mov     rax, rdi
 
 memset_loop:
-    cmp rdx, 0
-    je end
-    mov byte[rdi + rcx], sil
-    inc rcx
-    dec rdx
-    jmp memset_loop
+    cmp     rdx, 0
+    je      exit
+    mov     byte[rdi], cl
+    dec     rdx
+    inc     rdi
+    jmp     memset_loop
 
-end:
-    mov rax, rdi
-    ret
-
-exit_failure:
-
+exit:
     ret
